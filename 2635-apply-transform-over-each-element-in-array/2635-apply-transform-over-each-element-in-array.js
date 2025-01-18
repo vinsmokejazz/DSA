8e269e480d05a7a271f70fbbaa24838e670fd97a
VENU <vinsmokejazz@gmail.com>
2025-01-18 23:31:43 +0530

    Time: 53 ms (48.03%), Space: 49 MB (38.64%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const newArr=[];
+    for(let i=0;i<arr.length;i++){
+        newArr[i]=fn(arr[i],i);
+    }
+    return newArr;
+    
+};
